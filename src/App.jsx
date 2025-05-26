527969fa21ae7d2e2d2d48177b48e3bca273b32a
Sheikh Farid <mdfaridsagor101@gmail.com>
2025-05-26 10:50:44 +0600

    complete 4.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import Counter from "./components/Escape Hatches/Counter";
-import StopWatch from "./components/Escape Hatches/StopWatch";
+import { useRef } from "react";
 
 function App() {
+  const myRef = useRef(null);
+
+  const handleScroll = ()=>{
+    myRef.current.textContent = 'Hello React'
+  }
   return (
     <>
-     <StopWatch />
+      <div className="h-[3000px]">
+        <div className="w-10" ref={myRef}>Hello world</div>
+        <button className="mt-auto" onClick={handleScroll}>Scroll to Div</button>
+      </div>
     </>
   );
 }
